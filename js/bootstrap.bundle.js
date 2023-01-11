ad685276e025d021a0545cac25396eae69297821
awpdesigns <awpdesigns56@gmail.com>
2023-01-11 19:02:24 +0700

    Minor bug fixes

--- js/bootstrap.bundle.js
+++ js/bootstrap.bundle.js
@@ -13,7 +13,7 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.atasnalar = factory());
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.bootstrap = factory());
   })(this, (function () { 'use strict';
 
     /**
